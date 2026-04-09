a208e8bb83229f9bac5b2d304a2fc20020dcdf53
kateisdying <katelynm1022@gmail.com>
2026-04-09 16:13:43 -0500

    removed test case

--- serializer.py
+++ serializer.py
@@ -22,21 +22,3 @@ class Serializer:
 
     def to_json(self, quiz_node: QuizNode, indent: int = 2) -> str:
         return json.dumps(self.serialize(quiz_node), indent=indent)
-    
-if __name__ == "__main__":
-    from models import QuizNode, QuestionNode, OptionNode
-
-    quiz = QuizNode(
-        title="Sample Quiz",
-        questions=[
-            QuestionNode(
-                text="What is 2 + 2?",
-                options=[
-                    OptionNode(is_correct=False, text="3"),
-                    OptionNode(is_correct=True,  text="4"),
-                ]
-            )
-        ]
-    )
-
-    print(Serializer().to_json(quiz))    
