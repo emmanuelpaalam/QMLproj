72acb5776ef9adcec8e3ebe3fb2a2daf8e6f2969
kateisdying <katelynm1022@gmail.com>
2026-04-09 16:09:49 -0500

    serializer + test quiz file

--- serializer.py
+++ serializer.py
@@ -1,2 +1,42 @@
+import json
+from models import QuizNode
+
 class Serializer:
-    pass
+    def serialize(self, quiz_node: QuizNode) -> dict:
+        return {
+            "title": quiz_node.title,
+            "questions": [
+                {
+                    "text": question.text,
+                    "options": [
+                        {
+                            "text": option.text,
+                            "is_correct": option.is_correct
+                        }
+                        for option in question.options
+                    ]
+                }
+                for question in quiz_node.questions
+            ]
+        }
+
+    def to_json(self, quiz_node: QuizNode, indent: int = 2) -> str:
+        return json.dumps(self.serialize(quiz_node), indent=indent)
+    
+if __name__ == "__main__":
+    from models import QuizNode, QuestionNode, OptionNode
+
+    quiz = QuizNode(
+        title="Sample Quiz",
+        questions=[
+            QuestionNode(
+                text="What is 2 + 2?",
+                options=[
+                    OptionNode(is_correct=False, text="3"),
+                    OptionNode(is_correct=True,  text="4"),
+                ]
+            )
+        ]
+    )
+
+    print(Serializer().to_json(quiz))    
